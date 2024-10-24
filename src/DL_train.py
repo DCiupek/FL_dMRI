af73d3d66af14f3f101d86f697ff360218c1071b
Dominika Ciupek <62035783+DCiupek@users.noreply.github.com>
2024-10-24 11:27:01 +0200

    Add function to validate additional arguments

--- src/DL_train.py
+++ src/DL_train.py
@@ -7,20 +7,26 @@ Author: Dominika Ciupek
 import torch
 from torch.utils.data import DataLoader, random_split
 import pickle
-import sys
 from diffusion_data import ImagMRIDataset
 from models import ParamUNet
-from train_evaluate import train, predict
+from train_evaluate import train
 import wandb
-import matplotlib.pyplot as plt
-import numpy as np
 import os
 import argparse
 import warnings
 
 
+# Custom function to validate additional mandatory arguments
+def validate_args(args):
+    if args.wandb == 'Yes':
+        if args.key is None:
+            warnings.warn("Weights & Biases key not provided, you need to create a Weights & Biases account, or use an existing account", UserWarning)
+
 # Create the parser
 parser = argparse.ArgumentParser(description="Script to train the model using a single dataset.")
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Script to train the model using a single dataset.", formatter_class=argparse.RawTextHelpFormatter)
 
 # Add arguments
 parser.add_argument('parameter', type=str, help='Microstructural parameter, e.g. FA.')
@@ -31,11 +37,14 @@ parser.add_argument('batch', type=int, help='Batch size, e.g. 16.')
 parser.add_argument('learning_rate', type=float, help='Learning rate, e.g. 0.001.')
 parser.add_argument('dataset', type=str, help='Dataset name, e.g. CamCan.')
 parser.add_argument('save_path', type=str, help='Path to save the model, e.g. C:/Users/.')
-parser.add_argument('wandb', type=str, help='Use Weights & Biases for monitoring, Yes or No.')
-parser.add_argument('--key', type=str, help='Weights & Biases key, required if you want to use Weights & Biases.', required=False)
+parser.add_argument('wandb', type=str, help="Use Weights & Biases for monitoring, Yes or No.\n  - If 'Yes', the following arguments are required: --key.\n")
+parser.add_argument('--key', type=str, help='Weights & Biases key.', required=False)
 
 # Parse the arguments
 args = parser.parse_args()
+
+# Validate arguments based on wandb
+validate_args(args)
 
 # Set global parameters
 PARAMETER = args.parameter
@@ -58,10 +67,6 @@ L = 6
 
 # Set Weights & Biases
 if args.wandb == "Yes":
-    
-    # Check if key is entered
-    if args.key==None:
-        warnings.warn("Weights & Biases key not provided, you need to create a Weights & Biases account, or use an existing one", UserWarning)
 
     # Log in to Weights & Biases
     wandb.login(key = args.key)
